1a97eb9ec48a4d8e8ae74233a7c0e68e1056d048
Claiirou <clairemachet@hotmail.fr>
2022-04-11 17:11:07 +0200

    style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
   return (
     <header>
-      <nav>
-        <Link to="/">Home</Link>
+      <nav className="navbar">
+        <div className="navbarLogo"></div>
+        <ul className="navbar_links">
+          <li className="navbar_item">
+            <a href="/" className="navbar_link">Accueil</a>
+          </li>
+          <li className="navbar_item">
+            <a href="/" className="navbar_link">Catégorie</a>
+          </li>
+          <li className="navbar_item">
+            <a href="/" className="navbar_link">Score</a>
+          </li>
+          <li className="navbar_item">
+            <a href="/" className="navbar_link">About Us</a>
+          </li>
+        </ul>
+        <button className="navbar_burger">
+          <span className="burgerBar"></span>
+        </button>
       </nav>
     </header>
   );
 }
+
+//  <Link to="/">Home</Link>
+ 
