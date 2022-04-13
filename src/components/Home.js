fdcdaeda37e158dc2555d5b315f3dc6c166ed8cb
Dervieux <dervieux.ben@gmail.com>
2022-04-13 17:27:13 +0200

    move the restart button to the bottom of the result page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,15 @@
+import '../styles/home.css';
+
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className="homepage">
+      <h1>INQUIZ'ITOR</h1>
+      <div>
+        <button className="letsGo" type="button">
+          Let's Quizz
+        </button>
+      </div>
+      <div className="joke-of-the-day" />
+    </div>
+  );
 }
--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -3,25 +3,24 @@ import '../styles/ResultPage.css';
 
 function ResultPage() {
   return (
-    <div className="resultpage">
-      <h1 className="result-title">Bien joué</h1>
-      <div className="result-quiz">
+    <>
+      <div className="resultpage">
+        <h1 className="result-title">Bien joué</h1>
         <h2>7 / 10</h2>
+        <div className="animation-quiz">
+          <img
+            src={'https://media.giphy.com/media/l3q2XhfQ8oCkm1Ts4/giphy.gif'}
+            alt="bon score"
+          />
+        </div>
+        <h2 className="details-quiz">Détails de vos réponses</h2>
+        <div className="details" />
       </div>
-      <div>
-        <button className="restart" type="button">
-          Recommencer
-        </button>
-      </div>
-      <div className="animation-quiz">
-        <img
-          src={'https://media.giphy.com/media/l3q2XhfQ8oCkm1Ts4/giphy.gif'}
-          alt="bon score"
-        />
-      </div>
-      <h2 className="details-quiz">Détails de vos réponses</h2>
-      <div className="details" />
-    </div>
+
+      <button className="restart" type="button">
+        Recommencer
+      </button>
+    </>
   );
 }
 
