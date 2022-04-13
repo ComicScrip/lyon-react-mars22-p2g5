09e7afa79c592533c92cc93baf7e50f0bca9b187
Claiirou <clairemachet@hotmail.fr>
2022-04-13 17:34:08 +0200

    add new page for my link

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import ContactPage from './ContactPage';
+import ResultPage from './ResultPage';
+import QuizPage from './QuizPage';
 
 export default function Main() {
   return (
     <main>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="./components/ContactPage" element={<ContactPage />} />
+        <Route path="./components/ResultPage" element={<ResultPage />} />
+        <Route path="./components/QuizPage" element={<QuizPage />} />
       </Routes>
     </main>
   );
---
+++ src/components/QuizPage.jsx
@@ -0,0 +1,3 @@
+export default function QuizPage() {
+  return <div>toto</div>;
+}
