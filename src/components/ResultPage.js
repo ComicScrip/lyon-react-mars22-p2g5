3018b8f6d584f7859dc2ca0ea35fc531ec32dfe8
Dervieux <dervieux.ben@gmail.com>
2022-04-15 11:49:54 +0200

    update resultpage

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -3,24 +3,21 @@ import '../styles/ResultPage.css';
 
 function ResultPage() {
   return (
-    <>
-      <div className="resultpage">
-        <h1 className="result-title">Bien joué</h1>
-        <h2>7 / 10</h2>
-        <div className="animation-quiz">
-          <img
-            src={'https://media.giphy.com/media/l3q2XhfQ8oCkm1Ts4/giphy.gif'}
-            alt="bon score"
-          />
-        </div>
-        <h2 className="details-quiz">Détails de vos réponses</h2>
-        <div className="details" />
+    <div className="resultpage">
+      <h1 className="result-title">Bien joué</h1>
+      <h2>7 / 10</h2>
+      <div className="animation-quiz">
+        <img
+          src={'https://media.giphy.com/media/l3q2XhfQ8oCkm1Ts4/giphy.gif'}
+          alt="bon score"
+        />
       </div>
-
+      <h2 className="details-quiz">Détails de vos réponses</h2>
+      <div className="details" />
       <button className="restart" type="button">
         Recommencer
       </button>
-    </>
+    </div>
   );
 }
 
