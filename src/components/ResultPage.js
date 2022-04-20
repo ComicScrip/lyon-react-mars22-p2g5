19204eb1de39d9c540086c22e3baabeb8ee5b165
Dervieux <dervieux.ben@gmail.com>
2022-04-20 17:29:19 +0200

    result page is functional

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -1,16 +1,60 @@
 import React from 'react';
 import '../styles/ResultPage.css';
 
+const questions = [
+  {
+    question1: '1 + 1 = ?',
+    answer: [3, 4, 5],
+    correctAnswer: 2,
+  },
+  {
+    question2: 'couleur du soleil?',
+    answer: ['green', 'red', 'blue'],
+    correctAnswer: 'jaune',
+  },
+  {
+    question3: '2 + 2 = ?',
+    answer: [5, 7, 6],
+    correctAnswer: 4,
+  },
+];
+
+const answers = [2, 'jaune', 5];
+
+const animation = {
+  good: 'https://media.giphy.com/media/l3q2XhfQ8oCkm1Ts4/giphy.gif',
+  medium: 'https://media.giphy.com/media/sB4Z9U3yErt9S/giphy.gif',
+  bad: 'https://media.giphy.com/media/N2i9JoLDDI5VK/giphy.gif',
+};
+
+const resultTitle = {
+  great: 'Bien joué !!!',
+  middle: 'Pas trop mal',
+  wrong: 'Il y a du boulot',
+};
+
 function ResultPage() {
+  const countScore = (s, currentAnswer, index) =>
+    currentAnswer === questions[index].correctAnswer ? s + 1 : s;
+  const score = answers.reduce(countScore, 0);
+
+  function gifanim(s) {
+    if (s < 4) return animation.bad;
+    if (s < 7) return animation.medium;
+    return animation.good;
+  }
+
+  function resultSentence(s) {
+    if (s < 4) return resultTitle.wrong;
+    if (s < 7) return resultTitle.middle;
+    return resultTitle.great;
+  }
+
   return (
     <div className="resultpage">
-      <h1 className="result-title">Bien joué</h1>
-      <h2>7 / 10</h2>
+      <h1 className="result-title">{resultSentence(score)}</h1>
       <div className="animation-quiz">
-        <img
-          src={'https://media.giphy.com/media/l3q2XhfQ8oCkm1Ts4/giphy.gif'}
-          alt="bon score"
-        />
+        <img src={gifanim(score)} alt=" animation end quiz" />
       </div>
       <h2 className="details-quiz">Détails de vos réponses</h2>
       <div className="details" />
