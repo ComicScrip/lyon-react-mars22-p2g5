ada0b784a9e16635985201307d4a00101fee3629
JBillault <billaultjonathan@gmail.com>
2022-04-20 16:13:02 +0200

    all answers are stored, the questions list and the anwser list are transmited to the result page, code simplified

--- src/components/QuizAPI.jsx
+++ src/components/QuizAPI.jsx
@@ -15,10 +15,10 @@ function QuizAPI() {
     },
   ];
   const [quizQuestions, setQuizQuestions] = useState(sampleQuestion);
-  const [currentQuestion, setCurrentQuestion] = useState(sampleQuestion[0]);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(1);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const currentQuestion = quizQuestions[currentQuestionIndex];
   const [answerChoice, setAnswerChoice] = useState([]);
-  const [quizEnded, setQuizEnded] = useState(false);
+  const quizEnded = currentQuestionIndex === quizQuestions.length;
   // const [score, setScore] = useState(0);
   const getQuestions = () => {
     axios
@@ -26,28 +26,14 @@ function QuizAPI() {
       .then((response) => response.data)
       .then((data) => {
         setQuizQuestions(data.results);
-        setCurrentQuestion(data.results[0]);
       });
   };
   const handleChoiceAnswer = (value) => {
     setAnswerChoice((table) => [...table, value]);
     if (currentQuestionIndex < quizQuestions.length) {
-      setCurrentQuestionIndex((count) => count + 1);
-      setCurrentQuestion(quizQuestions[currentQuestionIndex]);
+      setCurrentQuestionIndex((index) => index + 1);
     }
-    if (currentQuestionIndex === quizQuestions.length) {
-      setQuizEnded(true);
-      for (let i = 0; i < quizQuestions.length; i += 1) {
-        console.log(answerChoice[i]);
-        console.log(quizQuestions[i].correct_answer);
-        // answerChoice.forEach(answer =>
-        // answer === quizQuestions[i].correct_answer ? setScore(count => count + 1) : '');
-      }
-    }
-    // console.log(score);
   };
-  console.log(quizQuestions);
-  console.log(answerChoice);
   //   const [timer, setTimer] = useState(20);
   //   useEffect(() => {
   //     let interval;
@@ -69,49 +55,52 @@ function QuizAPI() {
       ) : (
         <>
           {/* <ShowQuestions questionItem={quizQuestions[currentQuestion]} /> */}
-          <div className={quizEnded ? 'hiddenQuiz' : 'quiz'}>
-            <div className="question">
-              {he.decode(currentQuestion.question)}
+          {quizEnded ? (
+            <ResultDummy answer={answerChoice} questions={quizQuestions} />
+          ) : (
+            <div className={quizEnded ? 'hiddenQuiz' : 'quiz'}>
+              <div className="question">
+                {he.decode(currentQuestion.question)}
+              </div>
+              <div className="reponses">
+                <button
+                  type="submit"
+                  className="answers answerOne"
+                  onClick={(event) => handleChoiceAnswer(event.target.value)}
+                  value={currentQuestion.correct_answer}
+                >
+                  {currentQuestion.correct_answer}
+                </button>
+                <button
+                  type="submit"
+                  className="answers answerTwo"
+                  onClick={(event) => handleChoiceAnswer(event.target.value)}
+                  value={currentQuestion.incorrect_answers[0]}
+                >
+                  {currentQuestion.incorrect_answers[0]}
+                </button>
+                <button
+                  type="submit"
+                  className="answers answerThree"
+                  onClick={(event) => handleChoiceAnswer(event.target.value)}
+                  value={currentQuestion.incorrect_answers[1]}
+                >
+                  {currentQuestion.incorrect_answers[1]}
+                </button>
+                <button
+                  type="submit"
+                  className="answers answerFour"
+                  onClick={(event) => handleChoiceAnswer(event.target.value)}
+                  value={currentQuestion.incorrect_answers[2]}
+                >
+                  {currentQuestion.incorrect_answers[2]}
+                </button>
+              </div>
             </div>
-            <div className="reponses">
-              <button
-                type="submit"
-                className="answers answerOne"
-                onClick={(event) => handleChoiceAnswer(event.target.value)}
-                value={currentQuestion.correct_answer}
-              >
-                {currentQuestion.correct_answer}
-              </button>
-              <button
-                type="submit"
-                className="answers answerTwo"
-                onClick={(event) => handleChoiceAnswer(event.target.value)}
-                value={currentQuestion.incorrect_answers[0]}
-              >
-                {currentQuestion.incorrect_answers[0]}
-              </button>
-              <button
-                type="submit"
-                className="answers answerThree"
-                onClick={(event) => handleChoiceAnswer(event.target.value)}
-                value={currentQuestion.incorrect_answers[1]}
-              >
-                {currentQuestion.incorrect_answers[1]}
-              </button>
-              <button
-                type="submit"
-                className="answers answerFour"
-                onClick={(event) => handleChoiceAnswer(event.target.value)}
-                value={currentQuestion.incorrect_answers[2]}
-              >
-                {currentQuestion.incorrect_answers[2]}
-              </button>
-            </div>
-          </div>
-          {/* <ProgressBar bgColor="#b31212" completed={timer} /> */}
+          )}
+          ;{/* <ProgressBar bgColor="#b31212" completed={timer} /> */}
         </>
       )}
-      {quizEnded ? <ResultDummy /> : ''}
     </>
   );
 }
