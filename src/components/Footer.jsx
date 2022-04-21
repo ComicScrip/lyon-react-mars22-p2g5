d18171e27d7da8056219d3dd78e9f551e17ca9d6
Claiirou <clairemachet@hotmail.fr>
2022-04-21 16:59:39 +0200

    fixed size logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer>
       <Link to="/">
-        <img src={logo} alt="logo" className="logoImage" />
+        <img src={logo} alt="logo" className="logoImageFooter" />
       </Link>
       <p className="footerText">Inquiz'itor © 2022 Tous droits réservés.</p>
     </footer>
