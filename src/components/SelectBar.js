49d687ff042f75591a36ee2dcda6c10ed503b3b2
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-04-26 14:58:52 +0200

    linking api to categories

--- src/components/SelectBar.js
+++ src/components/SelectBar.js
@@ -1,54 +1,46 @@
-import Select from 'react-select';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Categories.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function SelectBar() {
-  const categories = [
-    { label: 'General Knowledge', value: 9 },
-    { label: 'Entertainment: Books', value: 10 },
-    { label: 'Entertainment: Film', value: 11 },
-    { label: 'Entertainment: Music', value: 12 },
-    { label: 'Entertainment: Musicals & Theatres', value: 13 },
-    { label: 'Entertainment: Television', value: 14 },
-    { label: 'Entertainment: Video Games', value: 15 },
-    { label: 'Entertainment: Board Games', value: 16 },
-    { label: 'Science & Nature', value: 17 },
-    { label: 'Science: Computers', value: 18 },
-    { label: 'Science: Mathematics', value: 19 },
-    { label: 'Mythology', value: 20 },
-    { label: 'Sports', value: 21 },
-    { label: 'Geography', value: 22 },
-    { label: 'History', value: 23 },
-    { label: 'Politics', value: 24 },
-    { label: 'Art', value: 25 },
-    { label: 'Celebrities', value: 26 },
-    { label: 'Animals', value: 27 },
-    { label: 'Vehicles', value: 28 },
-    { label: 'Entertainment: Comics', value: 29 },
-    { label: 'Science: Gadgets', value: 30 },
-    { label: 'Entertainment: Japanese Anime & Mangas', value: 31 },
-    { label: 'Entertainment: Cartoon & Animations', value: 32 },
+  const sampleCategory = [
+    {
+      id: '',
+      name: '',
+    },
   ];
 
-  const [selectedCat, setSelectedCat] = useState(categories.label);
+  const [selectedCat, setSelectedCat] = useState(sampleCategory);
+  /* const [results, setResults] = useState([]); */
 
-  const change = (e) => {
-    setSelectedCat(e.label, e.value);
-  };
+  useEffect(() => {
+    axios
+      .get('https://opentdb.com/api_category.php')
+      .then((res) => setSelectedCat(res.data.trivia_categories));
+  }, []);
 
   return (
     <div>
       <div className="select-bar">
-        <Select
-          options={categories}
-          onChange={change}
-          className="react-select"
-        />
+        <select
+          name="category-selection"
+          onChange={(e) => setSelectedCat(e.target.value)}
+          value={selectedCat}
+        >
+          <option key={''} value={''}>
+            --Choose a category--
+          </option>
+          {selectedCat.map((label) => (
+            <option key={label.id} value={label.name}>
+              {label.name}
+            </option>
+          ))}
+        </select>
       </div>
       <Link to="/QuizAPI">
         <div className={!selectedCat ? '' : 'category-box'}>
-          <h1>{!selectedCat ? '' : `Quizz ${selectedCat}`}</h1>
+          <h1>{`Quizz ${selectedCat.name}`}</h1>
         </div>
       </Link>
     </div>
