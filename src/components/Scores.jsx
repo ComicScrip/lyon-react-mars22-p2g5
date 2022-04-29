6ba87218167c568cfdcc2861963f1497152a62aa
Dervieux <dervieux.ben@gmail.com>
2022-04-29 14:56:58 +0200

    podium css updated

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../styles/scores.css';
-import or from '../assets/or.png';
+/* import or from '../assets/or.png';
 import argent from '../assets/argent.png';
-import bronze from '../assets/bronze.png';
+import bronze from '../assets/bronze.png'; */
 
 function Scores() {
   return (
@@ -10,18 +10,28 @@ function Scores() {
       <h1 className="scoreTitle">SCORE</h1>
       <div className="podium">
         <div className="third">
-          <img className="coupe" src={bronze} alt="coupe-bronze" />
+          {/* <img className="coupe" src={bronze} alt="coupe-bronze" /> */}
         </div>
         <div className="first">
-          <img className="coupe" src={or} alt="coupe-or" />
+          {/* <img className="coupe" src={or} alt="coupe-or" /> */}
         </div>
         <div className="second">
-          <img className="coupe" src={argent} alt="coupe-argent" />
+          {/* <img className="coupe" src={argent} alt="coupe-argent" /> */}
         </div>
       </div>
-      <div className="socle" />
-      <div className="scoreList" />
-      <h1>GAMERS LIST</h1>
+      <div className="socle">
+        <h2>Podium</h2>
+      </div>
+      <h1 className="scoreTitle">GAMERS LIST</h1>
+      <div className="scoreList">
+        <ul className="playersList">
+          {/* {score.map((player, user, score) => (
+          <li key={player}>
+            {user} : {score}
+          </li>
+        ))} */}
+        </ul>
+      </div>
     </div>
   );
 }
