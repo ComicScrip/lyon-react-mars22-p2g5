16e590e733f3e80d79d7ace1adfd0aa525f34815
Claiirou <clairemachet@hotmail.fr>
2022-05-03 17:29:57 +0200

    add popup

--- src/components/NewJoke.jsx
+++ src/components/NewJoke.jsx
@@ -1,10 +1,25 @@
 import React, { useState } from 'react';
 import '../styles/Popup.css';
 import '../styles/NewJoke.css';
+import Klaxon from '../assets/Klaxon.mp3';
+import swal from 'sweetalert';
 
 function NewJoke({ show, setShow }) {
   const [addNewQuestion, setAddNewQuestion] = useState('');
   const [addNewAnswer, setAddNewAnswer] = useState('');
+  // const [showPopup, setShowPopup] = useState(false);
+
+  const sonKlaxon = new Audio(Klaxon);
+
+  const start = () => {
+    sonKlaxon.play();
+  };
+
+  const handleClick = () => {
+    start();
+    // showPopup(!setShowPopup);
+    swal('Merci pour votre blague !');
+  };
 
   return (
     <div className={`popup ${show ? 'active' : ''} `}>
@@ -50,7 +65,7 @@ function NewJoke({ show, setShow }) {
           </div>
         </div>
         <div className="popup__footer">
-          <button type="button" className="btnGo bntJoke">
+          <button type="button" className="btnGo bntJoke" onClick={handleClick}>
             ENVOYER
           </button>
         </div>
